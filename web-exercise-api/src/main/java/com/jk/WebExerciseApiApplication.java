cf00ded28dc9c13adbc7a04418f97af9207d3402
sqx <939287903@qq.com>
2019-11-14 19:48:03 +0800

    第一次提交消费者框架

---
+++ web-exercise-api/src/main/java/com/jk/WebExerciseApiApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExerciseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebExerciseApiApplication.class, args);
+    }
+
+}
---
+++ web-provider-lh/src/main/java/com/jk/WebProviderLhApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProviderLhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProviderLhApplication.class, args);
+    }
+
+}
